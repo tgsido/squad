a08d688038b64e5fc7995050db0d6bb9e335dbb7
tgsido <osido@stanford.edu>
2019-03-01 13:54:20 -0800

    added print statements to help debug

--- models.py
+++ models.py
@@ -137,11 +137,11 @@ class BiDAF(nn.Module):
         c_mask = torch.zeros_like(cw_idxs) != cw_idxs
         q_mask = torch.zeros_like(qw_idxs) != qw_idxs
         c_len, q_len = c_mask.sum(-1), q_mask.sum(-1)
-        """
+
         print("cw_idxs.size()", cw_idxs.size())
         print("qw_idxs.size()", qw_idxs.size())
-        print("c_mask: ", c_mask)
-        print("q_mask: ", q_mask)
+        #print("c_mask: ", c_mask)
+        #print("q_mask: ", q_mask)
         print("c_mask.size(): ", c_mask.size())
         print("q_mask.size(): ", q_mask.size())
         print("c_len: ", c_len)
@@ -151,22 +151,25 @@ class BiDAF(nn.Module):
 
 
         print("bert_embeddings.size()" , bert_embeddings.size()) # (batch_size, max_context_len + max_question_len, 768)
-        """
+
         bert_c_emb = bert_embeddings[:,0:max_context_len,:]
         bert_q_emb = bert_embeddings[:,max_context_len:,:]
-        """
+
         print("bert_c_emb.size() ", bert_c_emb.size())
         print("bert_q_emb.size() ", bert_q_emb.size())
-        """
+
         c_emb = bert_c_emb[:,0:torch.max(c_len),:]
         q_emb = bert_q_emb[:,0:torch.max(q_len),:]
 
-        """
+
         print("c_emb.size() ", c_emb.size())
         print("q_emb.size() ", q_emb.size())
-        """
+
         c_enc = self.enc(c_emb, c_len)    # (batch_size, c_len, 2 * hidden_size)
         q_enc = self.enc(q_emb, q_len)    # (batch_size, q_len, 2 * hidden_size)
+
+        print("c_enc.size() ", c_enc.size())
+        print("q_enc.size() ", q_enc.size())
 
         att = self.att(c_enc, q_enc,
                        c_mask, q_mask)    # (batch_size, c_len, 8 * hidden_size)
