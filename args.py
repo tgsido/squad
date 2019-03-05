e721a185babbc49bdf15f98520255d9ec2e5ecff
tgsido <osido@stanford.edu>
2019-03-04 22:38:13 -0800

    eval every 25600 steps

--- args.py
+++ args.py
@@ -226,7 +226,7 @@ def get_train_args():
 
     parser.add_argument('--eval_steps',
                         type=int,
-                        default=6400, #changed to 5k for more evaluation
+                        default=25600, #changed to 5k for more evaluation
                         help='Number of steps between successive evaluations.')
     parser.add_argument('--lr',
                         type=float,
