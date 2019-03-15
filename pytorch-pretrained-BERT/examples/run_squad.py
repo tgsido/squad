ae1c9ba83b48eb7f3f0a495b993898613372588d
tgsido <osido@stanford.edu>
2019-03-15 00:30:58 -0700

    remove additional returns from model call

--- pytorch-pretrained-BERT/examples/run_squad.py
+++ pytorch-pretrained-BERT/examples/run_squad.py
@@ -1089,7 +1089,7 @@ def main():
                 if n_gpu == 1:
                     batch = tuple(t.to(device) for t in batch) # multi-gpu does scattering it-self
                 input_ids, input_mask, segment_ids, start_positions, end_positions = batch
-                _, _, loss = model(input_ids, segment_ids, input_mask, start_positions, end_positions)
+                loss = model(input_ids, segment_ids, input_mask, start_positions, end_positions)
                 if n_gpu > 1:
                     loss = loss.mean() # mean() to average on multi-gpu.
                 if args.gradient_accumulation_steps > 1:
