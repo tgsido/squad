fce32b0c97783ce42487fd0e70d3926e2a05cf31
tgsido <osido@stanford.edu>
2019-03-16 22:21:44 -0700

    break out of epoch every 10 steps

--- pytorch-pretrained-BERT/examples/run_squad.py
+++ pytorch-pretrained-BERT/examples/run_squad.py
@@ -1103,7 +1103,7 @@ def main():
         for epoch_num in trange(int(args.num_train_epochs), desc="Epoch"):
             for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
                 num_steps += 1
-                if num_steps == 10:
+                if num_steps % 10:
                     break
                 if n_gpu == 1:
                     batch = tuple(t.to(device) for t in batch) # multi-gpu does scattering it-self
